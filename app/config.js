56010240051fe416999bb00e26cca847f5afabe3
Debs <ddurojaiye@gmail.com>
2020-04-29 17:21:54 +0100

    Add files for v2

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Local plan',
+  serviceName: 'Local plans',
 
   // Default port that prototype runs on
   port: '3000',
