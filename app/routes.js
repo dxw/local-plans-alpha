eb85142dabacf2453ce7a6f9d91516c35eb7437a
Stuart Harrison <pezholio@gmail.com>
2020-04-30 16:29:20 +0100

    Get councils from a local file

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const utils = require('./utils')
 
 // Add your routes here - above the module.exports line
 
 router.get("/v2/organisation", function (req, res) {
-  utils.getCouncils(function(councils) {
-    res.render('v2/organisation', { councils: councils });
-  })
+  var councils = require('./data/councils.json')
+  res.render('v2/organisation', { councils: councils });
 })
 
 module.exports = router
