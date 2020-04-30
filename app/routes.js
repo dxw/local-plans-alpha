07005c5fb5558a9d3033f51ef03714602ff02542
Stuart Harrison <pezholio@gmail.com>
2020-04-30 15:57:27 +0100

    Add a util method to get a list of councils

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const utils = require('./utils')
 
 // Add your routes here - above the module.exports line
+
+router.get("/v2/organisation", function (req, res) {
+  utils.getCouncils(function(councils) {
+    res.render('v2/organisation', { councils: councils });
+  })
+})
 
 module.exports = router
